3442270af5332b0e34b746607a717ab30d126550
unknown <ganijustine8@gmail.com>
2024-10-17 19:23:42 +0800

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+let favSingers = ['Flow G','Al James', 'Bruno Mars'];
+
+console.log(favSingers[0]); // Output Flow G:  but if you enter 1 the output will be Al James
+
+let favNums = [17, 69, 6, 99];
+
+let mixedArray = ['string', ['otherarray'], 123, true];
+
+console.log(mixedArray[0]); // string
+console.log(mixedArray[1]); // [ 'otherarray' ]
+console.log(mixedArray[2]); // 123
+console.log(mixedArray[3]); // true
